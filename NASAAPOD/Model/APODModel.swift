364c0f4c828e74c9599e4d948bd6c9283b6225f9
emirmalika <emirmalika0712@gmail.com>
2023-02-10 16:42:18 +0600

    add mdoel and constants

---
+++ NASAAPOD/Model/APODModel.swift
@@ -0,0 +1,17 @@
+//
+//  APOD.swift
+//  NASAAPOD
+//
+//  Created by Malik Em on 10.02.2023.
+//
+
+import Foundation
+
+struct APOD {
+    let copyright: String?
+    let date: String?
+    let explanation: String?
+    let hdurl: String?
+    let title: String?
+}
+
