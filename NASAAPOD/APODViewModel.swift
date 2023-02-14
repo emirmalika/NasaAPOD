3d78b7b42ece8edb17e3227e371fd28344173bf1
emirmalika <emirmalika0712@gmail.com>
2023-02-14 15:38:11 +0600

    create an UI

---
+++ NASAAPOD/APODViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  APODViewModel.swift
+//  NASAAPOD
+//
+//  Created by Malik Em on 14.02.2023.
+//
+
+import Foundation
+
+
--- NASAAPOD/MainViewController.swift
+++ NASAAPOD/MainViewController.swift
@@ -8,13 +8,83 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    var dataResponse: DataResponse? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .red
+        configureView()
+        setConstraints()
     }
-
-
+    
+    private lazy var photo: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.backgroundColor = .systemGray
+        
+        return imageView
+    }()
+    
+    private lazy var titleOfPhoto: UIButton = {
+        let btn = UIButton()
+        
+        btn.setTitle("Hi", for: .normal)
+        btn.backgroundColor = .yellow
+        
+        return btn
+    }()
+    
+    private lazy var stackContentView: UIStackView = {
+        let sv = UIStackView(arrangedSubviews: [photo, titleOfPhoto])
+        
+        sv.axis = .vertical
+        sv.distribution = .fill
+        sv.spacing = 8.0
+        
+        return sv
+    }()
+    
+    private var menu = UIBarButtonItem(image: UIImage(systemName: "list.bullet"), style: .plain, target: MainViewController.self, action: #selector(menuBtnTapped))
+    
+    private var share = UIBarButtonItem(image: UIImage(systemName: "ellipsis"), style: .plain, target: MainViewController.self, action: #selector(shareBtnTapped))
+    
+    @objc func menuBtnTapped() {
+        
+    }
+    
+    @objc func shareBtnTapped() {
+        
+    }
+    
+    private func configureView() {
+        title = "hello"
+        
+        view.backgroundColor = .white
+        
+        navigationItem.leftBarButtonItem = menu
+        navigationItem.rightBarButtonItem = share
+        
+        menu.tintColor = .black
+        share.tintColor = .black
+    }
+    
+    private func setConstraints() {
+        [photo, titleOfPhoto].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        view.addSubview(stackContentView)
+        
+        stackContentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackContentView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0.0),
+            stackContentView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0.0),
+            stackContentView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0.0),
+            stackContentView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100.0)
+        ])
+        
+       
+    }
+    
 }
-
